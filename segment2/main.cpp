e9196956de6f74b034be6f810747eafc7238aca1
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-10-27 18:11:05 +0400

    Update main.cpp

--- segment2/main.cpp
+++ segment2/main.cpp
@@ -1,14 +1,10 @@
 #include <QGuiApplication>
-
 #include "packagereader.h"
-
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
-
 
     packageReader pckreader;
     if(!pckreader.initialize())
