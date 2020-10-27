06daf7df4aae60a31eacea2dfdfbb667827e7f49
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-10-27 17:31:53 +0400

    Update packagereader.h

--- segment2/packagereader.h
+++ segment2/packagereader.h
@@ -1,3 +1,13 @@
+/*******************************************************************************************************
+* Qt GUI Application
+* https://github.com/Engin-Boot/monitor-pi-board-s1b9/
+*
+* Copyright (c) 2020 Engin-Boot
+* Released under MIT License
+*
+* Refer project root for license text
+********************************************************************************************************/
+
 #ifndef PACKAGEREADER_H
 #define PACKAGEREADER_H
 #include<string>
